3c192c8fe08f63ab5adbf48b80c172441c432c1c
Titus Soporan <titus@tsoporan.com>
2015-03-09 22:12:18 -0400

    collect information from current tab, stage for push

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,9 +1,23 @@
 // Send out current tab to RememberThis.
 
-var codea = "";
+function pushPage(message) {
+	console.log('push page message', message);
+}
+
+function readPage(callback) {
+	chrome.tabs.executeScript(null, { 
+		file: 'injected.js',
+	});
+        
+        chrome.runtime.onMessage.addListener(function(message)  { 
+                callback(message); 
+        }); 	
+}
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: ''
-  });
+	chrome.runtime.getBackgroundPage(function(eventPage) {
+		console.log('event page', eventPage);
+
+		eventPage.readPage(pushPage);
+	});
 });
