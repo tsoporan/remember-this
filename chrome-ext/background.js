4d779dea749ca7361caf5fd066218c1e047489ce
Titus Soporan <titus@tsoporan.com>
2015-03-15 01:07:18 -0400

    successful ajax post from chrome ext to server

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,20 +1,68 @@
 // Send out current tab to RememberThis.
 
-function pushPage(message) {
-	console.log('push page message', message);
+var postTo = 'http://localhost:3000/capture'
+
+function ajax(options, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(options.method, options.url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(options.data));
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            return callback(xhr)
+        }
+    }
 }
 
+// Handle server response.
+function handleResponse(response) {
+    console.log('response', response);
+}
+
+function pushPage(message) {
+	console.log('push page message', message);
+
+    var now = new Date,
+        data = {
+            'title': message.title,
+            'url': message.url,
+            'body': message.body,
+            'date': now,
+            'uid' : 1,
+    };
+
+    // POST
+    console.log('making post to ', postTo, 'with', data);
+    ajax({
+        url: postTo,
+        method: 'POST',
+        data : data,
+    }, handleResponse);
+
+}
+
+// Reads the current page on the active tab.
 function readPage(callback) {
+
+    console.log('exectuting injected');
 	chrome.tabs.executeScript(null, { 
 		file: 'injected.js',
 	});
-        
-        chrome.runtime.onMessage.addListener(function(message)  { 
-                callback(message); 
-        }); 	
+
+    console.log('done');
+
+    console.log('on message');
+    chrome.runtime.onMessage.addListener(function(message)  { 
+
+        console.log('before callback message');
+        callback(message); 
+    }); 	
 }
 
+// Runs on extension click.
 chrome.browserAction.onClicked.addListener(function(tab) {
+
 	chrome.runtime.getBackgroundPage(function(eventPage) {
 		console.log('event page', eventPage);
 
